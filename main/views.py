1f2658b236bb541ba205eb2313153062797c699e
Adam <A.ergashev@internet.ru>
2024-01-15 18:29:54 +0300

    (hw 21.1) FBV -> CBV refactor

--- main/views.py
+++ main/views.py
@@ -1,43 +1,10 @@
+
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 
 from .models import Product, Contact
 
-
-#
-#
-# def index(request):
-#     product_list = Product.objects.order_by('pk')[:5]
-#     context = {
-#         'object_list': product_list,
-#         'title': 'Главная страница'
-#     }
-#     return render(request, 'main/index.html', context)
-#
-#
-# def show_item(request, pk):
-#     item = get_object_or_404(Product, pk=pk)
-#     context = {
-#         'item': item,
-#         'title': item
-#     }
-#     return render(request, 'main/item.html', context)
-#
-#
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'{name} ({email}): {message}')
-#     contact_list = [{
-#         'name': contact.name,
-#         'phone': contact.phone,
-#         'address': contact.address,
-#         'email': contact.email
-#     } for contact in Contact.objects.all()]
-#     return render(request, "main/contacts.html", {'title': 'Контакты', 'contacts': contact_list})
 
 class IndexView(ListView):
     model = Product
@@ -55,26 +22,24 @@ class ItemView(DetailView):
     extra_context = {'title': 'Наш крутой продукт'}
 
 
-
-
-class ContactsView(ListView):
+class ContactsView(FormView):
     template_name = 'main/contacts.html'
     form_class = Contact
     success_url = reverse_lazy('contacts')
     extra_context = {'title': 'Контакты'}
-    queryset = Contact.objects.all()
 
     def form_valid(self, form):
-        name = form.cleaned_data['name']
-        email = form.cleaned_data['email']
-        message = form.cleaned_data['message']
-        send_mail(
-            f'Сообщение от {name} ({email})',
-            message,
-            email,
-            ['youremail@example.com'],
-            fail_silently=False,
-        )
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail(
+                f'Сообщение от {name} ({email})',
+                message,
+                email,
+                ['youremail@example.com'],
+                fail_silently=False,
+            )
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
